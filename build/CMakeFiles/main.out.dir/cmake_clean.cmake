9f1af59c8e01dd9830fe26f86b16f44d108c11e8
rachione <lychnussys@gmail.com>
2022-09-19 21:18:18 -0400

    init

---
+++ build/CMakeFiles/main.out.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.out.dir/main.cpp.obj"
+  "CMakeFiles/main.out.dir/main.cpp.obj.d"
+  "build/main.out.exe"
+  "build/main.out.exe.manifest"
+  "build/main.out.pdb"
+  "libmain.out.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
